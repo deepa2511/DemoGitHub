d31a942539ae107f59c6f33f7666f9dc403a3bc7
deepa2511 <75685292+deepa2511@users.noreply.github.com>
2021-07-27 16:26:22 +0530

    Second commit

--- DemoGitHub/Program.cs
+++ DemoGitHub/Program.cs
@@ -7,6 +7,8 @@ namespace DemoGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            for(int i=1;i<10;i++)
+                  Console.WriteLine(i);
 
         }
     }
